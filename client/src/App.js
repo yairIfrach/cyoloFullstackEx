efe21c48b8f97fc2ca5b83b0d90eab842c9972ea
yairIfrach <yair.ifrach2000@gmail.com>
2023-06-26 17:57:19 +0300

    add client. dialog, main flow, snackbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
-import MainPage from './components/MainPage'
 import MainUploadComp from './components/MainUploadComp'
-import UploadForm from './components/UploadForm'
 
-
-function App() {
+const App = () => {
   return (
     <div className="App">
-      {/* <MainPage/> */}
       <MainUploadComp/>
     </div>
   );
--- client/src/components/MainUploadComp.js
+++ client/src/components/MainUploadComp.js
@@ -1,25 +1,48 @@
 import React, { useState } from 'react';
 import UploadForm from './UploadForm'
 import UploadMsgComp from './UploadMsgComp'
+import axios from 'axios';
+
+const DEFAULT_IMG = 'https://ik.imagekit.io/cyolostatamic/securing-connected-ot-environment-3.png';
 
 const MainUploadComp = () => {
+
     const [showModal, setShowModal] = useState(false);
     const [shareableURL, setShareableURL] = useState('');
+    const [backgroundImag, setBackgroundImag] = useState(DEFAULT_IMG);
+
+    const setBackgroungImg = async (url) => {
+        try {
+            const response = await axios.get(url);
+            const imageData = response.data.img.dataImg.data;
+            const mimeType = response.data.img.dataImg.mimetype;
+            const dataUrl = `data:${mimeType};base64,${imageData}`;
+            // const dataUrl = `data:${imgRes.mimetype};base64,${Buffer.from(imgRes.data, 'binary').toString('base64')}`;
+            //fix set background
+            setBackgroundImag(dataUrl)
+
+        } catch (error) {
+            console.error('Error uploading file:', error);
+        }
+    }
+
 
     const handleFileUploadSuccess = (url) => {
         setShareableURL(url);
         setShowModal(!showModal);
+        setBackgroungImg(url)
     };
 
     return (
-        <div>
-            {!showModal ?
-                <UploadForm handleFileUploadSuccess={handleFileUploadSuccess} />
-                : <UploadMsgComp
-                    showModal={showModal}
-                    shareableURL={shareableURL}
-                    setShowModal={setShowModal} />}
-        </div>
+        <>
+            <UploadForm
+                handleFileUploadSuccess={handleFileUploadSuccess}
+                backgroundImag={backgroundImag} />
+            {showModal ? <UploadMsgComp
+                showModal={showModal}
+                shareableURL={shareableURL}
+                setShowModal={setShowModal} /> : <></>}
+        </>
     );
 }
 
